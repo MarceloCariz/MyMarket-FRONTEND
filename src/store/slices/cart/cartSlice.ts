f63c43b02c7cd3929debc0f74767b490fb850e5b
MarceloCariz <marcejedd300@hotmail.com>
2023-05-22 19:43:31 -0400

    fix: remove localstorage

--- src/store/slices/cart/cartSlice.ts
+++ src/store/slices/cart/cartSlice.ts
@@ -34,7 +34,8 @@ export const cartSlice = createSlice({
                         return cart;
                     }
                     return cart
-                })
+                });
+                localStorage.setItem("cart", JSON.stringify(state.cart));
                 return state;
             }
 
@@ -45,16 +46,27 @@ export const cartSlice = createSlice({
         removeToCart: (state, action) => {
             const product = action.payload.product;
             const isExist = state.cart.find(({_id}) =>  _id === product._id);
-            
-            if(isExist){
-                isExist.quantity = isExist.quantity - 1;
-            }
 
-            if(isExist && isExist.quantity === 0){
+
+            if(isExist && isExist.quantity === 1){
                 state.cart = state.cart.filter(({_id}) =>  _id !== isExist._id);
                 localStorage.setItem("cart", JSON.stringify(state.cart));
                 return state;
             }
+
+            if(isExist){
+                state.cart =  state.cart.map((cart) => { 
+                    if(cart._id ===  product._id){
+                        cart.quantity = cart.quantity - 1;
+                        return cart;
+                    }
+                    return cart
+                });
+                localStorage.setItem("cart", JSON.stringify(state.cart));
+                return state;
+            }
+
+
             return state;
 
         }
