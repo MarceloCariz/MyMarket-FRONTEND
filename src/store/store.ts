b61af5a9c1a27f0f96c1b32348ebe45a4636976c
MarceloCariz <marcejedd300@hotmail.com>
2023-05-31 12:10:02 -0400

    Add: delte comments store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,4 @@
 import {configureStore} from '@reduxjs/toolkit';
-
-import { authSlice } from './slices/auth';
-import { uiSlice } from './slices/ui';
-import { productSlice } from './slices/product/productSlice';
-import { cartSlice } from './slices/cart';
 import rootReducer from './slices/rootReducer';
 
 
@@ -11,16 +6,10 @@ import rootReducer from './slices/rootReducer';
 
 export const store = configureStore({
     reducer: rootReducer,
-    // reducer:{
-    //     auth: authSlice.reducer,
-    //     product: productSlice.reducer,
-    //     cart: cartSlice.reducer,
-    //     ui: uiSlice.reducer
-    // }
-    // middleware: (getDefaultMiddleware) =>
-    // getDefaultMiddleware({
-    //     serializableCheck: false
-    // }),
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+        serializableCheck: false
+    }),
 })
 
 export type RootState = ReturnType<typeof store.getState>;
